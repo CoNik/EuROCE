4ba171e2208f9930c8b40dac6378d8bd45b13203
Andrea Micheloni <fanta02@m7i.org>
2020-04-26 17:23:31 +0200

    Fixed swagger UI, embedded v3 and edited its code to make it work.

--- main_api/swagger_server/__main__.py
+++ main_api/swagger_server/__main__.py
@@ -4,9 +4,11 @@ import connexion
 
 from swagger_server import encoder
 from flask_cors import CORS
+import os
 
 def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
+    options = {"swagger_path": os.path.join(os.path.dirname(os.path.realpath(__file__)),"..","..","swagger-ui"), "swagger_ui_config": {"openapi_spec_url": "http://localhost:8080/api/swagger.json"}}
+    app = connexion.App(__name__, specification_dir='./swagger/', options=options)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Fake News Hammer public API'})
     CORS(app.app)
