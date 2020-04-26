8752d19a8fd5796f2302348f5ee06fa6cb0ae51a
Andrea Micheloni <fanta02@m7i.org>
2020-04-26 17:34:46 +0200

    Moved the swagger UI to the subprojects, in order to embed it via Docker

--- main_api/swagger_server/__main__.py
+++ main_api/swagger_server/__main__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 import os
 
 def main():
-    options = {"swagger_path": os.path.join(os.path.dirname(os.path.realpath(__file__)),"..","..","swagger-ui"), "swagger_ui_config": {"openapi_spec_url": "http://localhost:8080/api/swagger.json"}}
+    options = {"swagger_path": os.path.join(os.path.dirname(os.path.realpath(__file__)),"..","swagger-ui"), "swagger_ui_config": {"openapi_spec_url": "http://localhost:8080/api/swagger.json"}}
     app = connexion.App(__name__, specification_dir='./swagger/', options=options)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Fake News Hammer public API'})
