cf8a4ddc129458da297262254f258043d0c0dd35
Andrea Micheloni <fanta02@m7i.org>
2020-04-26 23:03:56 +0200

    Added random but deterministic case generator for the API.

--- main_api/swagger_server/controllers/public_api_controller.py
+++ main_api/swagger_server/controllers/public_api_controller.py
@@ -1,12 +1,33 @@
 import connexion
 import six
 import datetime
+import random
 from swagger_server.models.case_file import CaseFile  # noqa: E501
 from swagger_server.models.dashboard_statistics import DashboardStatistics  # noqa: E501
+from swagger_server.models.cluster import Cluster  # noqa: E501
+from swagger_server.models.tag import Tag  # noqa: E501
 from swagger_server import util
 
 queries=0;
 queue=0;
+
+def get_random_casefile(random_int):
+    gen=random.Random(random_int);
+
+    random_urls=['maskanti-covid19.com', 'coronaviruschina.ml', 'anticovid-19.it', 'staysafecovid19.xyz', 'business-resource-facebook-covid-19.com', 'desinfectantes-covid19.com', 'zendesk-covid19.com', 'anticovid-19.co.uk', 'coronavirusprotection.io', 'covid19-medecinechinoise.ch', 'coronaviruspreventionkits.com', 'strongerthancoronavirus.com', 'covid19safety.shop', 'covid19taxreward.com', 'coronavirus-guidelines.online', 'covid19instatest.com', 'uberdoctorcoronavirus.com', 'ubercoronavirusprotection.com', 'againstcovid-19.com', 'coronavirusbeschermset.nl', 'cdc-gov.org', 'anti-covid19.fr', 'nocoronavirus.info', 'zendesk-covid19.net', 'stopcoronavirus.nl', 'byebyecoronavirus.com', 'coronavirusnewsnetwork.com', 'maskforcoronavirus.com', 'anticorona-virus.com', 'coronavirusfog.com', 'coronaviruspreppers.buzz', 'kabylie-stopcovid-19.com', 'zendesk-covid19.org', 'coronavirusconquerer.com', 'update-covid19.com', 'coronavirusgummy.com', 'aggiornadecretocovid19.online', 'coronaviruskingofbeers.com', 'covid19-products2020.com', 'covid19-testkit.net', 'coronavirusmaskpro.com.co', 'covid19.me.uk', 'bestmaskscoronavirus.com', 'anti-corona-virus.com', 'coronavirus-cure.store', 'terminixcovid19.com', 'coronavirus-total.ru', 'coronavirus-informations.online', 'covid-19-news.ru', 'coronavirusgen.com', 'italycovid-19.site', 'coronavirus2019.site', 'protectioncoronavirus.co.uk', 'runescape-covid-19.info', 'hmrc-refund-covid-19.com', 'coronavirushumano.org', 'n95coronavirusmaske.com', 'covid19-mask.com', 'securitycovid19.online', 'corona-virus-cure.xyz', 'covid19-symptoms.com', 'corona-virus-news.xyz', 'testcoronavirus.shop', 'covid19supply.ca', 'coronavirusmaskco.com', 'ent-covid19-gouv.info', 'coronavirusrations.co.uk', 'covid-19throatswab.com', 'anticovid19supply.com', 'examencoronavirus.cl', 'beatcovid19.co.uk', 'ameli-coronavirus.fr', 'recoverrryasitalycovid-19.xyz', 'netflix-coronavirus.com', 'desinfectantes-coronavirus.com', 'coronavirushealth.co', 'ameli-covid19.fr', 'promo-covid19-neftlix.ml', 'examencoronavirusadomicilio.cl', 'covid19-tests.info', 'bescherming-coronavirus.nl', 'coronavirusmart.com', 'safefromcovid-19.com', 'coronavirusrelief.net', 'cdc-coronavirus.com', 'coronavirusn95ppe.com', 'epidomacovid19.com', 'coronavirusapp.site', 'anticoronavirus.at', 'anti-covid19.shop', 'fuckcoronavirus.com', 'examenadomiciliocoronavirus.cl', 'theglobalcoronavirus.com', 'handsoapforcoronavirus.com', 'covid19tester.eu', 'covid-19comcast.org', 'covid19response.fund', 'thecovid19mask.co.za', 'coronavirusquicktests.com', 'facemask-coronavirus.com', 'covid-19donor.com', 'examencovid19.cl', 'coronavirusmasksuk.com', 'coronaviruspharma.com', 'mapacovid19.cl', 'covid-19comcast.com', 'united-kingdom-covid-19.com', 'coronavirustestkit.life', 'buycoronavirusfacemasks.com', 'coronavirus-19.lv', 'covid19crisis2020.com', 'sicurezza-covid19.com', 'bestmask-coronavirus.com', 'securitycovid19info.com', 'covid19masks.shop', 'covid-19cover.com', 'corona-virussafetymasks.com', 'covid-19facts.online', 'corona-virusss.com', 'viabcp-covid19.xyz', 'coronavirusinfor.world', 'coronavirus.uk', 'coronavirus19tests.com', 'hmrc-refund-covid19.com', 'coronavirus-apps.com', 'bell-covid19.com', 'coronacovid19information.online', 'coronavirusstatus.space', 'coronaviruspandemicessentials.com', 'shopcovid-19.com', 'verizoncovid-19.com', 'covid19-info.online', 'corona-virusapps.com', 'rogers-covid19.com', 'idscovid19.com', 'virys-covid19.ru', 'onavirus.cl']
+
+    random_paths=['news?article=%s', 'explore/%s/link.html?update=1', 'link/referrer?hl=en&trk=%s', 'corona-exposed.html#link-%s', 'art/2020-04-23-%s.html', 'COVID/news?art=%s,fn=10387', 't/%s']
+
+    random_clusters=[Cluster(id=4, name='fake-news-site', type='covid-misinformation'), Cluster(id=15, name='crypto-miner', type='malware-websites'), Cluster(id=12, name='extreme-content', type='covid-fake-cure'), Cluster(id=19, name='hidden-publisher', type='publicher-information')]
+
+    random_tags=[Tag(name='miracolous-cure',id=193), Tag(name='malvertising',id=29), Tag(name='onlineshop-promotion',id=98), Tag(name='promoting_hatred',id=25), Tag(name='discrimination',id=23), Tag(name='institution-distrust',id=29), Tag(name='conspiracy-theory',id=27), Tag(name='extreme-content',id=100)]
+
+    url="http://%s/%s" % (gen.choice(random_urls), gen.choice(random_paths) % gen.randint(1297,2804434));
+    name="Case file #%s" % random_int
+    clusters=gen.choices(random_clusters,k=gen.randint(0,2))
+    tags=gen.choices(random_tags,k=gen.randint(2,5))
+
+    return CaseFile(id=random_int, name=name, url=url, clusters=clusters, tags=tags);
 
 def get_case_file(UUID):  # noqa: E501
     """Retrieves a specific case file from its UUID
@@ -21,7 +42,14 @@ def get_case_file(UUID):  # noqa: E501
     global queries;
     queries = queries + 1;
 
-    return str(queries)
+    id_int=len(UUID)+102937
+
+    try:
+        id_int = int(UUID)
+    except ValueError:
+        id_int=sum(UUID.encode('utf-8'))+100000;
+
+    return get_random_casefile(id_int);
 
 
 def get_dashboard_stats():  # noqa: E501
