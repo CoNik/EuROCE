1b256562e3d30f423cd31399cbf978eedf2162ac
Andrea Micheloni <fanta02@m7i.org>
2020-04-26 13:12:34 +0200

    Fixed some values for the demo

--- proxy_osint_api/swagger_server/__main__.py
+++ proxy_osint_api/swagger_server/__main__.py
@@ -3,13 +3,14 @@
 import connexion
 
 from swagger_server import encoder
-
+from flask_cors import CORS
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Proxy OSINT API'})
-    app.run(port=8080)
+    CORS(app.app)
+    app.run(port=8080,host='localhost')
 
 
 if __name__ == '__main__':
